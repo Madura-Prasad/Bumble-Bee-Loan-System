8d8d95aa79cf9814840d4bafde74c21c0df28222
Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>
2023-09-23 04:14:59 +0530

    Delete Bumblebee-Back-End directory

--- Bumblebee-Back-End/src/main/java/com/example/demo/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Users;
-
-public interface UsersRepository extends JpaRepository<Users, Long>{
-
-}
