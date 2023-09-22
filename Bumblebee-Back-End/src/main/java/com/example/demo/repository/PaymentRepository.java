1f404c74aaff74794ae1f9008c9b145669f2291e
Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>
2023-09-23 04:21:44 +0530

    Delete Bumblebee-Back-End directory

--- Bumblebee-Back-End/src/main/java/com/example/demo/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-
-}
